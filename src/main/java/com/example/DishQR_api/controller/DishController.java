2ef37b785fec9ed23c3aa69052bfc4775eab631c
Piotr Duda <piotr.duda00@gmail.com>
2023-10-19 15:13:20 +0200

    authentication feature

--- src/main/java/com/example/DishQR_api/controller/DishController.java
+++ src/main/java/com/example/DishQR_api/controller/DishController.java
@@ -3,6 +3,7 @@ package com.example.DishQR_api.controller;
 import com.example.DishQR_api.model.Dish;
 import com.example.DishQR_api.service.DishService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class DishController {
     private final DishService dishService;
 
     @GetMapping(path = "/getAllDishes")
+    @PreAuthorize("hasRole('ADMIN')") // todo usunąć, tylko do testów
     public List<Dish> getAllDishes(){
         return dishService.getAllDishes();
     }
---
+++ src/main/java/com/example/DishQR_api/model/User.java
@@ -0,0 +1,70 @@
+package com.example.DishQR_api.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Document("users")
+public class User implements UserDetails {
+
+    @Id
+    String id;
+
+    String firstName;
+
+    String lastName;
+
+    @Indexed(unique = true)
+    String email;
+
+    String password;
+
+    Role role;
+
+    LocalDateTime createdAt;
+
+    LocalDateTime updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
